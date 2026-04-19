2059a412912b70eb9e7e99debca060cf4fb4e652
byadems <81581944+byadems@users.noreply.github.com>
2026-04-19 16:19:00 +0300

    Update ai.js

--- plugins/ai.js
+++ plugins/ai.js
@@ -23,7 +23,6 @@
     "gemini-3.1-flash-lite-preview",
     "gemini-3-flash-preview",
     "gemini-2.5-flash-lite",
-    "gemini-2.5-flash-lite-preview-09-2025",
     "gemini-2.5-flash",
     "gemini-2.5-pro"
   ];
